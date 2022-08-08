699f8f8b6589ecae0b80532d0c7d29316a3be8d0
Saurav438c <saurav438c@gmail.com>
2022-08-08 13:55:39 +0530

    assingment is done

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const cab = require('../logger/logger')
 const bca=require('../validater/formatter')
+const lodash=require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
@@ -9,6 +10,17 @@ router.get('/test-me', function (req, res) {
     console.log('‘Welcome to my application. I am <name> and a part of FunctionUp Plutonium cohort, on console')
     abc.printName()
     cab.Welcome()
+    const months= ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  let result=lodash.chunk(months,4)
+  console.log(result)
+  const odd=[1,3,25,87,98,83,87,69,85,37]
+  let x=lodash.tail(odd,1)
+  console.log(x)
+
+
+
     
 
     res.send('My second ever api!')
