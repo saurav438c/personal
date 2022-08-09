e34acadd167bea76e52f6f7b421a622122abf095
Saurav438c <saurav438c@gmail.com>
2022-08-09 19:37:42 +0530

    routes  modified

--- src/routes/route.js
+++ src/routes/route.js
@@ -47,6 +47,46 @@ router.get('/candidates', function(req, res){
     res.send(candidates)
 })
 
+router.get("/sol1", function (req, res) {
+  console.log('Query paramters for this request are '+JSON.stringify(req.query))
+  function getMissingNo(a, n) {
+  
+    let total = Math.floor((n + 1) * (n + 2) / 2);
+    for (let i = 0; i < n; i++)
+        total -= a[i];
+    return total;
+}
+
+// Driver Code
+
+let arr = [ 1, 2, 3, 5,6,7 ];
+let N = arr.length;
+let missingNumber = getMissingNo(arr, N);
+console.log(missingNumber);
+     res.send(  { data: missingNumber  }  );
+})
+router.get("/sol2", function (req, res) {
+  console.log('Query paramters for this request are '+JSON.stringify(req.query))
+  let arr= [33, 34, 35, 37, 38]
+   let len= arr.length
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let firstDigit= arr[0]
+   let lastDigit= arr.pop()
+   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+   let missingNumber= consecutiveSum - total
+  
+   res.send(  { data: missingNumber  }  );
+ });
+
+
+
+
+
 // use path param
 router.get('/candidates/:canidatesName', function(req, res){
     console.log('The request objects is '+ JSON.stringify(req.params))
